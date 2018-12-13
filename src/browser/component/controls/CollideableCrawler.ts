545c8566bd369e609e16c631f2f6fedd88c11a19
Tommi S.E. Laukkanen <tommi.s.e.laukkanen@gmail.com>
2018-12-13 07:53:41 +0200

    Moved arcade controls inside project and translated to typescript.

---
+++ src/browser/component/controls/CollideableCrawler.ts
@@ -0,0 +1,72 @@
+import {Object3D} from "three";
+
+export class CollidableCrawler {
+    self: Object3D;
+    root: Object3D;
+
+    children: Array<Object3D>;
+    queuedChildren: Array<Array<Object3D>>;
+    collidablesFinal: Array<Object3D>;
+    collideablesTemporary: Array<Object3D>;
+    queuedChildrenIndex: number;
+    childrenIndex: number;
+    initialCrawlDone: boolean;
+
+    constructor(self: Object3D,root: Object3D) {
+        this.self = self;
+        this.root = root;
+        this.queuedChildren = [];
+        this.queuedChildren.push(this.root.children);
+        this.queuedChildrenIndex = 0;
+        this.children = [];
+        this.childrenIndex = 0;
+        this.collidablesFinal = [];
+        this.collideablesTemporary = [];
+        this.initialCrawlDone = false;
+    }
+
+    start() {
+        this.crawl()
+    }
+
+    collideables() {
+        return this.collidablesFinal;
+    }
+
+    fullCrawl() {
+        while (!this.crawl()) ;
+    }
+
+    crawl() {
+        if (!this.initialCrawlDone) {
+            this.initialCrawlDone = true;
+            this.fullCrawl();
+            return;
+        }
+        if (this.children.length > this.childrenIndex) {
+            const current = this.children[this.childrenIndex];
+            this.childrenIndex++;
+            if (current === this.self) {
+                return;
+            }
+            this.queuedChildren.push(current.children);
+            if (current.type === 'Mesh') {
+                this.collideablesTemporary.push(current)
+            }
+        } else {
+            if (this.queuedChildren.length > this.queuedChildrenIndex) {
+                this.children = this.queuedChildren[this.queuedChildrenIndex];
+                this.childrenIndex = 0;
+                this.queuedChildrenIndex++;
+            } else {
+                this.queuedChildren = [];
+                this.queuedChildren.push(this.root.children);
+                this.queuedChildrenIndex = 0;
+                this.collidablesFinal = this.collideablesTemporary;
+                this.collideablesTemporary = [];
+                return true;
+            }
+        }
+        return false;
+    }
+}
--- src/browser/index.ts
+++ src/browser/index.ts
@@ -2,10 +2,12 @@ import {registerAFrameComponent} from "./AFrame";
 import {LabelComponent} from "./LabelComponent";
 import {IdentityComponent} from "./IdentityComponent";
 import {DataspaceComponent} from "./DataspaceComponent";
+import {ArcadeControls} from "./component/controls/ArcadeControls";
 
 registerAFrameComponent(() => new DataspaceComponent());
 registerAFrameComponent(() => new IdentityComponent());
 registerAFrameComponent(() => new LabelComponent());
+registerAFrameComponent(() => new ArcadeControls());
 
 
 
