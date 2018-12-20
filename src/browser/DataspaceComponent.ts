0a805a23d6615a18c813f22094696430e996dedb
Tommi S.E. Laukkanen <tommi.s.e.laukkanen@gmail.com>
2018-12-20 09:54:11 +0200

    Implemented transmitting avatar orientation.

--- src/browser/DataspaceComponent.ts
+++ src/browser/DataspaceComponent.ts
@@ -2,7 +2,7 @@ import {AbstractComponent} from "./AFrame";
 import {ClusterClient, Decode, Encode} from "@tlaukkan/aframe-dataspace";
 import uuid = require("uuid");
 import {DynamicSpace} from "./DynamicSpace";
-import {Object3D} from "three";
+import {Matrix4, Object3D, Plane, Quaternion, Vector3} from "three";
 import {Entity} from "aframe";
 import {StaticSpace} from "./StaticSpace";
 import {EntityStateEventDetail} from "./model/EntityStateEventDetail";
@@ -13,6 +13,8 @@ export class DataspaceComponent extends AbstractComponent {
     private avatarId = uuid.v4();
     private playerElement: Entity | null = null;
     private playerObject: Object3D | undefined = undefined;
+    private cameraElement: Entity | undefined;
+    private cameraObject: Object3D | undefined;
     private client: ClusterClient | undefined = undefined;
     private url: string | undefined = undefined;
     private dynamicSpace: DynamicSpace | undefined = undefined;
@@ -20,6 +22,13 @@ export class DataspaceComponent extends AbstractComponent {
 
     private lastRefresh: number = 0;
     private idToken: string | undefined;
+
+    private yAxisPositive = new Vector3(0, 1, 0);
+    private xzPlane = new Plane(this.yAxisPositive);
+    private cameraDirection: Vector3 = new Vector3(0, 0, 0);
+    private xzDirection: Vector3 = new Vector3(0, 0, 0);
+    private directionMatrix = new Matrix4();
+    private directionQuaternion = new Quaternion();
 
     constructor() {
         super(
@@ -84,15 +93,24 @@ export class DataspaceComponent extends AbstractComponent {
         console.log(this.name + " play");
         if (this.playerElement && !this.playerObject) {
             this.playerObject = this.playerElement!!.object3D;
+            let cameraElement = this.playerElement!!.querySelector('[camera]') as Entity;
+            this.cameraElement = cameraElement;
+            if (this.cameraElement) {
+                this.cameraObject = this.cameraElement.object3D;
+            }
+
             if (!this.playerObject) {
                 console.log("No player object.");
             }
+            if (!this.cameraObject) {
+                console.log("No camera object.");
+            }
         }
 
-        if (this.url && this.playerObject) {
+        if (this.url && this.playerObject && this.cameraObject) {
 
             this.client = new ClusterClient(this.url!!, this.avatarId, this.playerObject.position.x, this.playerObject.position.y, this.playerObject.position.z,
-                this.playerObject.quaternion.x, this.playerObject.quaternion.y, this.playerObject.quaternion.z, this.playerObject.quaternion.w, "<a-sphere></a-sphere>", this.idToken!!);
+                this.cameraObject.quaternion.x, this.cameraObject.quaternion.y, this.cameraObject.quaternion.z, this.cameraObject.quaternion.w, '<a-entity gltf-model="#robot" scale="0.3 0.3 0.3" avatar=""></a-entity>', this.idToken!!);
             this.client.onReceive = (serverUrl: string, type: string, message: string[]) => {
                 //console.log(message);
                 if (type === Encode.ADDED) {
@@ -147,10 +165,19 @@ export class DataspaceComponent extends AbstractComponent {
         if (this.client) {
             this.dynamicSpace!!.simulate(timeDelta / 1000);
             if (time - this.lastRefresh > 200) {
-                if (this.playerObject) {
+                if (this.playerObject && this.cameraObject) {
                     if (this.client.clusterConfiguration) {
+
+                        this.cameraObject.getWorldDirection(this.cameraDirection);
+                        //this.cameraDirection.multiplyScalar(-1);
+                        this.xzPlane.projectPoint(this.cameraDirection, this.xzDirection);
+                        this.xzDirection.normalize();
+
+                        this.directionMatrix.lookAt(new Vector3(0,0,0), this.xzDirection,this.yAxisPositive);
+                        this.directionQuaternion.setFromRotationMatrix(this.directionMatrix);
+
                         this.client!!.refresh(this.playerObject.position.x, this.playerObject.position.y, this.playerObject.position.z,
-                            this.playerObject.quaternion.x, this.playerObject.quaternion.y, this.playerObject.quaternion.z, this.playerObject.quaternion.w);
+                            this.directionQuaternion.x, this.directionQuaternion.y, this.directionQuaternion.z, this.directionQuaternion.w);
                     }
                 }
                 this.lastRefresh = time;
