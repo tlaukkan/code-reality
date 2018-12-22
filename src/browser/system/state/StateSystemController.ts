fc9609c9f9876870477b3657aa1cd5a48408fd78
Tommi S.E. Laukkanen <tommi.s.e.laukkanen@gmail.com>
2018-12-22 17:29:45 +0200

    Implemented removal of state when static or dynamic entity is removed.

--- src/browser/system/state/StateSystemController.ts
+++ src/browser/system/state/StateSystemController.ts
@@ -3,7 +3,7 @@ import {AbstractSystemController} from "../AbstractSystemController";
 
 export class StateSystemController extends AbstractSystemController {
 
-    private providers: Map<string, () => any> = new Map();
+    private factories: Map<string, () => any> = new Map();
     private states: Map<Entity, Map<string, any>> = new Map();
 
     constructor(system: System, scene: Scene, data: any) {
@@ -25,28 +25,37 @@ export class StateSystemController extends AbstractSystemController {
     tick(time: number, timeDelta: number): void {
     }
 
-    registerStateType<T>(type: string, provider: () => T) {
-        this.providers.set(type, provider);
+    registerStateFactory<T>(state: string, factory: () => T) {
+        this.factories.set(state, factory);
     }
 
-    getState<T>(entity: Entity, type: string): T {
-        if (!this.providers.has(type)) {
-            throw new Error("Unknown state type: " + type);
+    getState<T>(entity: Entity, state: string): T {
+        if (!this.factories.has(state)) {
+            throw new Error("Unknown state type: " + state);
         }
         if (!this.states.has(entity)) {
             this.states.set(entity, new Map());
         }
         const entityStates = this.states.get(entity)!!;
-        if (!entityStates.has(type)) {
-            entityStates.set(type, this.providers.get(type)!!());
+        if (!entityStates.has(state)) {
+            entityStates.set(state, this.factories.get(state)!!());
+            console.log(entity.tagName + " added state: " + state);
         }
-        return entityStates.get(type)!!;
+        return entityStates.get(state)!!;
     }
 
     removeStates(entity: Entity): void {
         this.states.delete(entity);
         console.log(entity.tagName + " removed states.");
     }
+
+    removeState(entity: Entity, state: string): void {
+        if (this.states.has(entity)) {
+            this.states.get(entity)!!.delete(state);
+        }
+        console.log(entity.tagName + " removed state: " + state);
+    }
+
 
 }
 
