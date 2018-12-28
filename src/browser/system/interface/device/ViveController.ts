e3a25a183a54edc94b8bdc8af69081d79039e9bb
Tommi S.E. Laukkanen <tommi.s.e.laukkanen@gmail.com>
2018-12-28 15:37:46 +0200

    Implementing vive controller movement.

--- src/browser/system/interface/device/ViveController.ts
+++ src/browser/system/interface/device/ViveController.ts
@@ -7,43 +7,52 @@ import {Button} from "../model/Button";
 
 export class ViveController extends AbstractComponentController implements Device {
 
+    private deviceSlot: DeviceSlot = DeviceSlot.PRIMARY_HAND;
+    private toolSlot: ToolSlot = ToolSlot.PRIMARY;
 
     constructor(component: Component, entity: Entity, data: any) {
-        super("vive-controller", {}, false, component, entity, data);
+        super("vive-controller", {
+            hand: {type: 'string', default: "right"},
+        }, false, component, entity, data);
 
         if (!component) {
             return;
         }
-        this.interfaceSystemController.setDevice(DeviceSlot.KEYBOARD_AND_MOUSE, this);
     }
 
     init(): void {
         console.log(this.componentName + " init");
 
+        if (this.data.hand == 'left') {
+            this.deviceSlot = DeviceSlot.SECONDARY_HAND;
+            this.toolSlot = ToolSlot.SECONDARY;
+        }
+        this.interfaceSystemController.setDevice(this.deviceSlot, this);
+
         this.addEventListener("triggerup", (detail: any) => {
-            this.interfaceSystemController.buttonUp(this, ToolSlot.SECONDARY, Button.TRIGGER);
+            this.interfaceSystemController.buttonUp(this, this.toolSlot, Button.TRIGGER);
             console.log("triggerup " + detail);
         });
         this.addEventListener("triggerdown", (detail: any) => {
-            this.interfaceSystemController.buttonDown(this, ToolSlot.SECONDARY, Button.TRIGGER);
+            this.interfaceSystemController.buttonDown(this, this.toolSlot, Button.TRIGGER);
             console.log("triggerdown " + detail);
         });
 
         this.addEventListener("gripup", (detail: any) => {
-            this.interfaceSystemController.buttonUp(this, ToolSlot.SECONDARY, Button.GRIP);
+            this.interfaceSystemController.buttonUp(this, this.toolSlot, Button.GRIP);
             console.log("gripup " + detail);
         });
         this.addEventListener("gripdown", (detail: any) => {
-            this.interfaceSystemController.buttonDown(this, ToolSlot.SECONDARY, Button.GRIP);
+            this.interfaceSystemController.buttonDown(this, this.toolSlot, Button.GRIP);
             console.log("gripdown " + detail);
         });
 
         this.addEventListener("menuup", (detail: any) => {
-            this.interfaceSystemController.buttonUp(this, ToolSlot.SECONDARY, Button.MENU);
+            this.interfaceSystemController.buttonUp(this, this.toolSlot, Button.MENU);
             console.log("menuup " + detail);
         });
         this.addEventListener("menudown", (detail: any) => {
-            this.interfaceSystemController.buttonDown(this, ToolSlot.SECONDARY, Button.MENU);
+            this.interfaceSystemController.buttonDown(this, this.toolSlot, Button.MENU);
             console.log("menudown " + detail);
         });
 
@@ -58,12 +67,12 @@ export class ViveController extends AbstractComponentController implements Devic
 
         this.addEventListener("axismove", (detail:  any) => {
             const axis: Array<number> = detail.axis;
-            console.log("axismove " + axis);
-            this.interfaceSystemController.stickTwist(this, ToolSlot.SECONDARY, axis[1], axis[0]);
+            console.log(this.toolSlot + " axismove " + axis);
+            this.interfaceSystemController.stickTwist(this, this.toolSlot, axis[1], axis[0]);
         });
-        this.addEventListener("trackpadchanged", (detail: any) => {
+        /*this.addEventListener("trackpadchanged", (detail: any) => {
             console.log("trackpadchanged " + JSON.stringify(detail));
-        });
+        });*/
     }
 
     update(data: any, oldData: any): void {
