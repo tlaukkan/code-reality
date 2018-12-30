30f4c709e35d30248e27e74f5a4f56f6848ad087
Tommi S.E. Laukkanen <tommi.s.e.laukkanen@gmail.com>
2018-12-30 11:05:40 +0200

    Tuning npm packaging.

---
+++ src/browser.ts
@@ -0,0 +1,20 @@
+export {registerComponentController, registerSystemController} from "./browser/AFrame";
+export {LabelController} from "./browser/component/label/LabelController";
+export {IdentityController} from "./browser/component/identity/IdentityController";
+export {KeyboardAndMouseDevice} from "./browser/system/interface/device/KeyboardAndMouseDevice";
+export {AnimatorController} from "./browser/component/animation/AnimatorController";
+export {AvatarController} from "./browser/component/avatar/AvatarController";
+export {SpaceSystemController} from "./browser/system/space/SpaceSystemController";
+export {registerStateFactory, StateSystemController} from "./browser/system/state/StateSystemController";
+export {States} from "./browser/model/States";
+export {MovementState} from "./browser/model/MovementState";
+export {InterfaceSystemController} from "./browser/system/interface/InterfaceSystemController";
+export {InterfaceController} from "./browser/system/interface/InterfaceController";
+export {MovementTool} from "./browser/system/interface/tool/MovementTool";
+export {ViveControllerDevice} from "./browser/system/interface/device/ViveControllerDevice";
+export {ExampleController} from "./browser/component/ExampleController";
+export {ExampleSystemController} from "./browser/system/ExampleSystemController";
+export {EntityTool} from "./browser/system/interface/tool/EntityTool";
+export {CollidableController} from "./browser/component/collidable/CollidableController";
+export {QuaternionController} from "./browser/component/quaternion/QuaternionController";
+
