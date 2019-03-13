54a6412488ba588d7239c7211ba9f8ea92a1dc23
Tommi Laukkanen <tommi.s.e.laukkanen@gmail.com>
2019-03-13 20:37:56 +0200

    Tuning anisotropy.

--- src/browser/three/gltf_load.ts
+++ src/browser/three/gltf_load.ts
@@ -84,7 +84,7 @@ let loading = false;
 
 function setMaterialAnisotropy(material: MeshBasicMaterial) {
     if (material.map) {
-        material.map.anisotropy = 1;
+        material.map.anisotropy = 2;
         //material.map.minFilter = LinearMipMapNearestFilter;
     }
 }
